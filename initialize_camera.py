1ab53c37d304a4d45a843f796461ec8006178377
bhumika kothari <bhumika.25bai10951@vitbhopal.ac.in>
2026-03-26 15:07:02 +0530

    Update initialize_camera.py

--- initialize_camera.py
+++ initialize_camera.py
@@ -6,3 +6,11 @@ def initialize_camera():
         print("Camera not working")
         return None
     return camera
+
+def capture_gray_frame(camera):
+    ret, frame = camera.read()
+    if not ret:
+        return None, None
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return frame, gray_frame
