c633bfaa78d89a310c5a4edf0077836ec074be01
bhumika kothari <bhumika.25bai10951@vitbhopal.ac.in>
2026-03-27 13:42:51 +0530

    Update detect_movement.py

--- detect_movement.py
+++ detect_movement.py
@@ -1,6 +1,6 @@
 def detect_movement(previous_frame, current_gray):
     if previous_frame is None:
-        return None, "STARTING...", (255, 255, 0)
+        return None, "STARTING", (255, 255, 0)
 
     difference = cv2.absdiff(previous_frame, current_gray)
     _, threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)
